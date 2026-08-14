b7df20f9e2f482b2a5a484786cc0eefe1fd725d0
YellowLemonCupcake <lozadabenjay@gmail.com>
2026-08-14 14:39:53 +0800

    feat: add log out button to Logs component

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+
 export default function Logs() {
-   return <h1 className="text-4xl">Hi</h1>;
+   return (
+      <>
+         <h1 className="text-4xl">Hi</h1>
+         <button
+            onClick={() => {
+               signOut();
+            }}
+         >
+            Log Out
+         </button>
+      </>
+   );
 }
